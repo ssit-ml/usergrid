448c0c5bd52ce090d33f13d071e8be1043cca67e
Jingyi Xiao <jingyi.it@gmail.com>
2020-05-15 16:46:03 +0800

    bugfix
    bugfix

--- stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/serialization/impl/shard/impl/NodeShardCacheImpl.java
+++ stack/corepersistence/graph/src/main/java/org/apache/usergrid/persistence/graph/serialization/impl/shard/impl/NodeShardCacheImpl.java
@@ -200,10 +200,10 @@ public class NodeShardCacheImpl implements NodeShardCache {
                 //wait for a trip to cassandra
                 .refreshAfterWrite( graphFig.getShardCacheTimeout(), TimeUnit.MILLISECONDS )
 
-                        //set our weight function, since not all shards are equal
-                .maximumWeight(MAX_WEIGHT_PER_ELEMENT * graphFig.getShardCacheSize() ).weigher( new ShardWeigher() )
+                // bugfix by jingyi
+                .maximumSize(graphFig.getShardCacheSize())
 
-                        //set our shard loader
+                //set our shard loader
                 .build( new ShardCacheLoader() );
     }
 
